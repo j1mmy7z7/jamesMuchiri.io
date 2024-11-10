68e1ee12f875c63c722ac5dad2a6a854e00a21d6
jmuchiri <mmjames451@gmail.com>
2024-11-10 22:34:19 +0300

    feat: add eventlistener to the button to toggle themes

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let darkmode = localStorage.getItem('dark-mode')
+
+const enableDarkmode = () => {
+  document.body.classList.add('dark-mode')
+  localStorage.setItem('dark-mode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('dark-mode')
+  localStorage.setItem('dark-mode', null)
+}
+
+if(darkmode === 'active') enableDarkmode()
+
+const themebtn = document.getElementById('theme')
+themebtn.addEventListener("click", () => {
+  darkmode = localStorage.getItem('dark-mode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
